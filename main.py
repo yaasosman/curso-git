4c818165a8e2e2b1ed02870babb0ae1db661e4af
Yasmin <yasminosman94@gmail.com>
2025-03-25 15:26:13 -0300

    Função imprime pares

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ def imprime_pares(n):
         if i % 2==0:
             print(i)
 
+def imprime_impares(n):
+    for i in range(0, n):
+        if i % 2!=0:
+            print(i)
+
 if __name__=='__main__':
     imprime_pares(10)
+
 
 
 
